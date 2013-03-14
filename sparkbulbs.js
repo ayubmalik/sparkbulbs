013f98f22a5cb228b7d955264b583b9a83b24143
Ayub Malik <malika06@mc-s083289.local>
2013-03-14 10:23:59 +0000

    Added json file

--- sparkbulbs.js
+++ sparkbulbs.js
@@ -1,9 +1,18 @@
+var db = {}; // Only use global var as a hack!
+
 var keywords = [
                 {"start":10, "end": 19, "keyword": "curiosity"}, 
                 {"start":20, "end": 30, "keyword": "curiosity"}, 
                 {"start":45, "end": 50, "keyword": "martian"}, 
                 {"start":60, "end": 70, "keyword": "space"}
                 ];
+
+var getData = function() {
+	$.get('sparkbulbs-data.json', function(data) {
+		db = data;
+		console.log('got data:', db);
+	});
+};
 
 var isIn = function(time, start, end) {
 	if (time >= start && time <= end) {
@@ -17,7 +26,7 @@ var isIn = function(time, start, end) {
 var sparkLookup = function() {
 	var player = this;
 	var cueTime = player.currentTime().toFixed(2);
-	var matches = jQuery.grep(keywords, function(elem, i) {
+	var matches = jQuery.grep(db.captions, function(elem, i) {
 		return isIn(cueTime, elem.start, elem.end);
 	});
 	
@@ -32,8 +41,6 @@ var showBulb = function() {
 	var offset = $("#sparkbulb-player").offset();
 	var imgLeft = (offset.left + $("#sparkbulb-player").width()) - 64;
 	var imgTop = (offset.top + $("#sparkbulb-player").height()) - 110;
-	
-//	console.log("pos", imgTop, imgLeft);
 	
 	$("#eureka").css({
 	       position:'absolute',
@@ -60,6 +67,7 @@ var bindClicks = function() {
 };
 
 _V_("sparkbulb-player").ready(function() {
+	getData();
 	var myPlayer = this;
 	myPlayer.addEvent("timeupdate", sparkLookup);
 	myPlayer.play();
